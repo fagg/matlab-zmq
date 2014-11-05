0b26e133b7570b05b76e4ca20b574eba7e7e4803
Anderson Bravalheri <andersonbravalheri@gmail.com>
2014-11-05 13:13:52 -0200

    Modified test_zmq_bind to test zmq_unbind

--- tests/test_zmq_bind.m
+++ tests/test_zmq_bind.m
@@ -3,10 +3,15 @@ function test_zmq_bind
     ctx = zmq_ctx_new();
     socket = zmq_socket(ctx, 'ZMQ_PUB');
 
+    %% binding
     assert_throw('EPROTONOSUPPORT', @zmq_bind, socket, 'abc://localhost'); % invalid transport
     assert_throw('EINVAL', @zmq_bind, socket, 'tcp://localhost');          % port must specified
     response = assert_does_not_throw(@zmq_bind, socket, 'tcp://127.0.0.1:30000');
+    assert(response == 0, 'status code should be 0, %d given.', response);
 
+    %% unbinding
+    assert_throw(@zmq_unbind, socket, 'tcp://127.0.0.1:30103');
+    response = assert_does_not_throw(@zmq_unbind, socket, 'tcp://127.0.0.1:30000');
     assert(response == 0, 'status code should be 0, %d given.', response);
 
     % close session
