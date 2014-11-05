fe644f5d7abfb4de21803918cde472b0c99eebc4
Anderson Bravalheri <andersonbravalheri@gmail.com>
2014-11-05 12:58:22 -0200

    Added zmq_disconnect

---
+++ src/core/zmq_disconnect.c
@@ -0,0 +1,41 @@
+#include <util/socket.h>
+#include <util/conversions.h>
+#include <util/errors.h>
+#include <mex.h>
+#include <zmq.h>
+
+void mexFunction(int nlhs, mxArray *plhs[], int nrhs, const mxArray *prhs[])
+{
+    void* socket = NULL;
+    char* endpoint = NULL;
+    int rc;
+
+    if (nrhs != 2) {
+        mexErrMsgIdAndTxt("zmq:disconnect:invalidArgs",
+                "Error: Two arguments are required: socket, endpoint.");
+        return;
+    }
+    if (mxIsChar(prhs[1]) != 1) {
+        mexErrMsgIdAndTxt("zmq:disconnect:invalidEndpoint",
+                "Error: endpoint is not a string.");
+        return;
+    }
+    if (mxGetM(prhs[1]) != 1) {
+        mexErrMsgIdAndTxt("zmq:disconnect:endpointNotRowVec",
+                "Error: endpoint is not a row vector.");
+        return;
+    }
+
+    socket = pointer_from_m(prhs[0]);
+    if (socket == NULL) return;
+
+    endpoint = (char*) str_from_m(prhs[1]);
+    if (endpoint == NULL) return;
+
+    rc = zmq_disconnect(socket, endpoint);
+
+    if (rc < 0) handle_error();
+    else plhs[0] = int_to_m((void*) &rc);
+
+    mxFree(endpoint);
+}
