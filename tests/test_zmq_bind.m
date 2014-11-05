dd5fb9f8b28f70370825c0d1d12b11c835160b88
Anderson Bravalheri <andersonbravalheri@gmail.com>
2014-11-05 13:24:45 -0200

    Generalized assertions in test_zmq_bind

--- tests/test_zmq_bind.m
+++ tests/test_zmq_bind.m
@@ -1,10 +1,10 @@
 function test_zmq_bind
     % let's just create and destroy a dummy socket
     ctx = zmq_ctx_new();
-    socket = zmq_socket(ctx, 'ZMQ_REP');
+    socket = zmq_socket(ctx, 'ZMQ_PUB');
 
-    assert_throw('zmq:bind:EPROTONOSUPPORT', @zmq_bind, socket, 'abc://localhost'); % invalid transport
-    assert_throw('zmq:bind:EINVAL', @zmq_bind, socket, 'tcp://localhost');          % port must specified
+    assert_throw('EPROTONOSUPPORT', @zmq_bind, socket, 'abc://localhost'); % invalid transport
+    assert_throw('EINVAL', @zmq_bind, socket, 'tcp://localhost');          % port must specified
     response = assert_does_not_throw(@zmq_bind, socket, 'tcp://127.0.0.1:30000');
 
     assert(response == 0, 'status code should be 0, %d given.', response);
