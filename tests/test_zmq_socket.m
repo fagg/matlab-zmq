a1dc31302843743ad1a291fd85300299d154e08d
Anderson Bravalheri <andersonbravalheri@gmail.com>
2014-11-06 08:57:54 -0200

    Changed identation of test_socket to adhere to coding style

--- tests/test_zmq_socket.m
+++ tests/test_zmq_socket.m
@@ -1,12 +1,12 @@
 function test_zmq_socket
-  ctx = zmq_ctx_new();
+    ctx = zmq_ctx_new();
 
-  % let's just create and destroy a dummy socket
-  socket = assert_does_not_throw(@zmq_socket, ctx, 'ZMQ_REP');
-  response = assert_does_not_throw(@zmq_close, socket);
-  assert(response == 0, 'status code should be 0, %d given.', response);
+    % let's just create and destroy a dummy socket
+    socket = assert_does_not_throw(@zmq_socket, ctx, 'ZMQ_REP');
+    response = assert_does_not_throw(@zmq_close, socket);
+    assert(response == 0, 'status code should be 0, %d given.', response);
 
-  % close session
-  zmq_ctx_shutdown(ctx);
-  zmq_ctx_term(ctx);
+    % close session
+    zmq_ctx_shutdown(ctx);
+    zmq_ctx_term(ctx);
 end
