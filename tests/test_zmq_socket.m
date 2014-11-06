941fd618b562aa484c710dd1190ed39f2804faad
Anderson Bravalheri <andersonbravalheri@gmail.com>
2014-11-05 22:17:47 -0200

    Included return status test in test_zmq_socket

--- tests/test_zmq_socket.m
+++ tests/test_zmq_socket.m
@@ -4,6 +4,7 @@ function test_zmq_socket
   % let's just create and destroy a dummy socket
   socket = assert_does_not_throw(@zmq_socket, ctx, 'ZMQ_REP');
   response = assert_does_not_throw(@zmq_close, socket);
+  assert(response == 0, 'status code should be 0, %d given.', response);
 
   % close session
   zmq_ctx_shutdown(ctx);
