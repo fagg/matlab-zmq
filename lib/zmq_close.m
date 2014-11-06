2a05283e8393790d9d53d3778140e091379fea6b
Anderson Bravalheri <andersonbravalheri@gmail.com>
2014-11-05 22:50:01 -0200

    Added zmq_close

---
+++ lib/zmq_close.m
@@ -0,0 +1,8 @@
+% zmq_close- Close a ZMQ socket
+%
+% Usage: status = zmq_close(socket)
+%
+% Input: socket - Instantiated ZMQ socket handle to be closed (see zmq_socket).
+%
+% Output: Zero if successful, otherwise -1.
+%
