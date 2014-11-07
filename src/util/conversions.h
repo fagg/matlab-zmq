ab7f34ad96f0242915a99725632f2f0bca8862ed
Anderson Bravalheri <andersonbravalheri@gmail.com>
2014-11-07 20:02:03 -0200

    Added size_t conversions

--- src/util/conversions.h
+++ src/util/conversions.h
@@ -10,6 +10,7 @@ mxArray* int_matrix_to_m(void* handle, size_t m, size_t n);
 
 mxArray* uint8_array_to_m(void* handle, size_t n);
 
+mxArray* size_t_to_m(void* handle);
 mxArray* uint64_to_m(void* handle);
 mxArray* int64_to_m(void* handle);
 mxArray* uint32_to_m(void* handle);
@@ -19,6 +20,7 @@ mxArray* pointer_to_m(void* handle);
 
 void* uint8_array_from_m(const mxArray* param, size_t n);
 
+void* size_t_from_m(const mxArray* param);
 void* uint64_from_m(const mxArray* param);
 void* int64_from_m(const mxArray* param);
 void* uint32_from_m(const mxArray* param);
