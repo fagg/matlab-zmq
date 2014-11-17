5c2e141b245455ca095b315e691b1de7bedadd6f
Ashton Fagg <ashton@fagg.id.au>
2014-11-17 21:27:26 +1000

    Updated examples for repackaged core API

--- examples/collector.m
+++ examples/collector.m
@@ -4,23 +4,23 @@ function collector
     % Example borrowed from
     % http://learning-0mq-with-pyzmq.readthedocs.org/en/latest/pyzmq/patterns/pushpull.html
 
-    context = zmq_ctx_new();
-    socket  = zmq_socket(context, 'ZMQ_PULL');
+    context = zmq.core.ctx_new();
+    socket  = zmq.core.socket(context, 'ZMQ_PULL');
     address = 'tcp://127.0.0.1:5558';
-    zmq_bind(socket, address);
+    zmq.core.bind(socket, address);
 
     total = 0;
     for x = 1:50
-        result = sscanf(char(zmq_recv(socket)), '%d %d');
+        result = sscanf(char(zmq.core.recv(socket)), '%d %d');
         fprintf('data received from consumer#%d: %d\n', result(1), result(2));
         total = total + result(2);
     end
 
     fprintf('\nTotal: %d\n', total);
 
-    zmq_disconnect(socket, address);
-    zmq_close(socket);
+    zmq.core.disconnect(socket, address);
+    zmq.core.close(socket);
 
-    zmq_ctx_shutdown(context);
-    zmq_ctx_term(context);
+    zmq.core.ctx_shutdown(context);
+    zmq.core.ctx_term(context);
 end
--- examples/producer.m
+++ examples/producer.m
@@ -4,19 +4,19 @@ function producer
     % Example borrowed from
     % http://learning-0mq-with-pyzmq.readthedocs.org/en/latest/pyzmq/patterns/pushpull.html
 
-    context = zmq_ctx_new();
-    socket  = zmq_socket(context, 'ZMQ_PUSH');
+    context = zmq.core.ctx_new();
+    socket  = zmq.core.socket(context, 'ZMQ_PUSH');
     address = 'tcp://127.0.0.1:5557';
-    zmq_bind(socket, address);
+    zmq.core.bind(socket, address);
 
     % NOICE: Start your result manager and workers before you start your producers
     for num = 1:100
-        zmq_send(socket, uint8(num2str(num)));
+        zmq.core.send(socket, uint8(num2str(num)));
     end
 
-    zmq_disconnect(socket, address);
-    zmq_close(socket);
+    zmq.core.disconnect(socket, address);
+    zmq.core.close(socket);
 
-    zmq_ctx_shutdown(context);
-    zmq_ctx_term(context);
+    zmq.core.ctx_shutdown(context);
+    zmq.core.ctx_term(context);
 end
