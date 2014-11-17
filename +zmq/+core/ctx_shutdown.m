4a280cc4ad89dc22abd9d746fbaf2241437c346f
Ashton Fagg <ashton@fagg.id.au>
2014-11-17 21:18:01 +1000

    Fixed function prototypes.

---
+++ +zmq/+core/ctx_shutdown.m
@@ -0,0 +1,7 @@
+% zmq_ctx_shutdown - Shutdown a ZMQ context.
+%
+% Usage: zmq_ctx_shutdown(context)
+%
+% Input: context - Instantiated ZMQ context handle (see zmq_ctx_new).
+%
+% Output: None.
---
+++ +zmq/+core/version.m
@@ -0,0 +1,5 @@
+% zmq_version - Retrieve ZMQ version.
+%
+% Usage:
+%   zmq_version;     % Prints ZMQ version to MATLAB console.
+%   v = zmq_version; % Return version string (e.g. '4.0.4').
