a2df9c0f6c1e7e8a30702ca5e9049f5c4f282718
Anderson Bravalheri <andersonbravalheri@gmail.com>
2014-11-19 20:04:13 -0200

    Better docs [MINOR]

--- lib/+zmq/@Socket/bind.m
+++ lib/+zmq/@Socket/bind.m
@@ -2,7 +2,7 @@ function bind(obj, endpoint)
     status = zmq.core.bind(obj.socketPointer, endpoint);
     if (status == 0)
         % Add endpoint to the tracked bindings
-        % this is import to the cleanup process
+        % this is important to the cleanup process
         obj.bindings{end+1} = endpoint;
     end
 end
--- lib/+zmq/@Socket/close.m
+++ lib/+zmq/@Socket/close.m
@@ -1,6 +1,6 @@
 function close(obj, endpoint)
     status = zmq.core.close(obj.socketPointer);
     if (status == 0)
-        obj.socketPointer = 0;
+        obj.socketPointer = 0; % ensure NULL pointer
     end
 end
