9a65afb347e1a2a4e0578b94b1dda36bb8d8e799
Anderson Bravalheri <andersonbravalheri@gmail.com>
2014-11-20 02:34:38 -0200

    Refactored context, to track socket creation

--- lib/+zmq/@Context/Context.m
+++ lib/+zmq/@Context/Context.m
@@ -1,23 +1,27 @@
 classdef Context < handle
-   
+
     properties (Access = private)
-        contextPointer = [];
+        contextPointer;
+        spawnedSockets;
     end
-    
+
     methods
-           function obj = Context(varargin)
-               if (nargin ~= 0)
-                   warning('zmq:Context:extraConstructArgs','Extraneous constructor arguments.');
-               end
-               obj.contextPointer = zmq.core.ctx_new();
-           end
-          
-           function delete(obj)
-              zmq.core.ctx_shutdown(obj.contextPointer);
-              zmq.core.ctx_term(obj.contextPointer);
-           end
+        function obj = Context(varargin)
+            if (nargin ~= 0)
+                warning('zmq:Context:extraConstructArgs','Extraneous constructor arguments.');
+            end
+            % Core API
+            obj.contextPointer = zmq.core.ctx_new();
+            % Initi properties
+            obj.spawnedSockets = {};
+        end
+
+        function delete(obj)
+            zmq.core.ctx_shutdown(obj.contextPointer);
+            zmq.core.ctx_term(obj.contextPointer);
+        end
     end
-    
+
     methods (Access = protected)
         normalized = normalize_const_name(obj, name);
     end
