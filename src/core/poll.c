e9bd62256f40c68a92d00088d00c74898f17c0bd
Ashton Fagg <ashton@fagg.id.au>
2014-12-03 22:27:23 +1000

    Fixed merge conflict.

--- src/core/poll.c
+++ src/core/poll.c
@@ -7,15 +7,9 @@
 
 void mexFunction(int nlhs, mxArray *plhs[], int nrhs, const mxArray *prhs[])
 {
-<<<<<<< HEAD
     void *socket = NULL;
     int rc;
     void *retRevents = NULL, *retEvents = NULL;
-=======
-    void* socket = NULL;
-    long timeout = NULL;
-    int rc, revents, events;
->>>>>>> 3b827dcd8fcbd8520ffab4ba99714d56c23c628c
     zmq_pollitem_t item;
 
     if (nrhs == 1) {
@@ -26,28 +20,17 @@ void mexFunction(int nlhs, mxArray *plhs[], int nrhs, const mxArray *prhs[])
     }
 
     item.socket = (void *) socket;
-<<<<<<< HEAD
     
     /* -1 for timeout blocks indefinitely until something happens. */
     rc = zmq_poll(&item, 1, -1);
-=======
-    rc = zmq_poll(&item, 1);
->>>>>>> 3b827dcd8fcbd8520ffab4ba99714d56c23c628c
     if (rc != 1) {
             handle_error();
             return;
     } else {
-<<<<<<< HEAD
             retRevents = (void *) &item.revents;
             retEvents = (void *) &item.events;
             plhs[0] = int_to_m(retRevents);
             plhs[1] = int_to_m(retEvents);
-=======
-            revents = (int) item.revents[0];
-            events = (int) item.events[0];
-            plhs[0] = int_to_m(revents);
-            plhs[1] = int_to_m(events);
->>>>>>> 3b827dcd8fcbd8520ffab4ba99714d56c23c628c
             return;
     }
 }
